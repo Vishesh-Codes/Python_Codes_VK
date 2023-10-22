491ee7581bd9aec5ab1f77cf17147f6f73c06cbc
Vishesh-Codes <visheshkarnabcd@gmail.com>
2023-10-22 16:06:24 +0545

    added simple programming problems file

---
+++ simpleProblems.py
@@ -0,0 +1,48 @@
+#=============================================
+# SIMPLE PROBLEMS SOLVED BY PYTHON PROGRAMMING
+#=============================================
+
+
+
+
+#----------------------------------
+# whether a number is odd or even
+#----------------------------------
+
+# # take input an integer
+# num = int(input("enter an integer value: "))
+
+# # check if it is odd or even
+# if num % 2 == 0:
+#     print(f"{num} is an even number.")
+# else: print(f"{num} is an odd number.")
+
+
+
+#---------------------------------------------------
+# whether 'num1' fall in the table of 'num2' or not
+#---------------------------------------------------
+
+# # take input for the two integers
+# print("enter the value of a and b")
+# a, b = int(input("a = ")), int(input("b = "))
+
+# # check if lies in the table or not and print
+# if a % b == 0:
+#     print(f"Yes. {a} lies in the table of {b}.")
+# else: print(f"No. {a} does not fall in the table of {b}.")
+
+
+
+#--------------------------------------
+# finding the sum of n natural numbers
+#--------------------------------------
+
+# # take input for the first natural numbers
+# num = int(input("Enter a natural number: "))
+# summ = 0
+# # increment the value of variable summ 
+# for i in range(num + 1):
+#     summ = summ + i
+# # print the output
+# print(f"The sum of first {num} natural numbers = {summ}")
